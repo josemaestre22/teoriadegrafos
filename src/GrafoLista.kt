8517de52a207b0c9f82d0c4ca1211b4580483463
Jose Maestre <joseandresmaestre@gmail.com>
2025-11-13 17:14:54 -0400

    Need to fix edge Labeling

--- src/GrafoLista.kt
+++ src/GrafoLista.kt
@@ -1,17 +1,117 @@
 import java.io.File
+import kotlin.inc
 
 class GrafoLista (val nombreArchivo: String)
 {
     val ady = abrirArchivo()
-    val color = CharArray(numVertices()) {'B'} // Cada posicion representa el color de ese vertice 'B' blanco, 'G' Gris, 'N' Negro
-    val distancia = IntArray(numVertices()) {0}
-    val cuentaAristas = IntArray(3) {0} // Cada posicion representa el numero de aristas de cada tipo [0] Arbol [1] Retorno [2] Avance o Cruzadas
-    val predecesor = IntArray(numVertices()) {-1}
-    val origen = 0 // Vertice de el cual parten los recorridos
     var esDirigido = 0
-    var cuenta = 0 // Numero de vertices visitados en un recorrido
 
-    //TODO: Implementar recorridos como Nested Class con Inheritance (Recorrido Anchura y Reccorrido Profundidad heredan de Recorrido)
+    inner class Recorrido (val origen: Int = 0)
+    {
+        val marcados = CharArray(numVertices()) {'B'} // Cada posicion representa el color con el que ese vertice esta marcado: 'B' blanco, 'G' Gris, 'N' Negro
+        val distancia = IntArray(numVertices()) {-1}
+        val cuentaAristas = IntArray(3) {0} // Cada posicion representa el numero de aristas de cada tipo [0] Arbol [1] Retorno [2] Avance o Cruzadas
+        val predecesor = IntArray(numVertices()) {-1}
+        var cuenta = 0 // Numero total de vertices visitados en un recorrido
+
+        fun clasificarAristas()
+        {
+            println("Arbol: ${cuentaAristas[0]}")
+            println("Retorno: ${cuentaAristas[1]}")
+            println("Avance/Cruzada: ${cuentaAristas[2]}\n")
+        }
+
+        fun mostrarCamino(destino: Int)
+        {
+            if (destino == origen)
+                print(origen)
+            else
+            {
+                if (predecesor[destino] == -1)
+                {
+                    println("no existe camino desde $destino al origen\n")
+                }
+                else
+                {
+                    mostrarCamino(predecesor[destino])
+                    print("->")
+                    print(destino)
+                }
+            }
+        }
+
+        fun profundidad(origen: Int = this.origen)
+        {
+            marcados[origen] = 'G' //  Marcarlo como siendo visitado
+            cuenta++
+
+            for (v in Adyacentes(origen))
+            {
+                if (marcados[v] == 'B') // No ha sido visitado
+                {
+                    cuentaAristas[0]++
+                    predecesor[v] = origen
+                    distancia[v] = distancia[origen] + 1
+                    profundidad(v)
+                }
+                else {
+                    if (marcados[v] == 'G' && origen == predecesor[v]) {
+                        cuentaAristas[1]++
+                    }
+                    else if (marcados[v] == 'N')
+                    {
+                        cuentaAristas[2]++
+                    }
+                }
+            }
+
+            marcados[origen] = 'N' // Marcarlo como Visitado
+        }
+
+        fun anchura(origen: Int = this.origen)
+        {
+            val cola = ArrayDeque<Int>()
+            var  v: Int
+            var adyacentes: List<Int>
+
+            marcados[origen] = 'G'
+            distancia[origen] = 0
+            cola.addLast(origen)
+
+            while (cola.isNotEmpty())
+            {
+                v = cola.removeFirst()
+                adyacentes = Adyacentes(v)
+
+                for (w in adyacentes)
+                {
+                    if (marcados[w] == 'B')
+                    {
+                        marcados[w] = 'G'
+                        cuenta++
+                        cuentaAristas[0]++
+                        predecesor[w] = v
+                        distancia[w] = distancia[v] + 1
+                        cola.addLast(w)
+                    }
+                    else if (w != predecesor[v])
+                    {
+                        if (marcados[w] == 'G' && predecesor[v] != predecesor[w])
+                        {
+                            cuentaAristas[1]++
+                        }
+                        else if (marcados[w] == 'N')
+                        {
+                            cuentaAristas[2]++
+                        }
+                    }
+                }
+
+                marcados[v] = 'N'
+            }
+        }
+    }
+
 
     fun numVertices(): Int {
         return ady.size
@@ -109,122 +209,5 @@ class GrafoLista (val nombreArchivo: String)
             }
         }
         println()
-    }
-
-    fun recorridoProfundidad(origen: Int = this.origen)
-    {
-        color[origen] = 'G' //  Marcarlo como siendo visitado
-        cuenta++
-
-        for (v in Adyacentes(origen))
-        {
-            if (color[v] == 'B') // No ha sido visitado
-            {
-                cuentaAristas[0]++
-                predecesor[v] = origen
-                distancia[v] = distancia[origen] + 1
-                recorridoProfundidad(v)
-            }
-            else {
-                if (color[v] == 'G' && origen == predecesor[v]) {
-                    cuentaAristas[1]++
-                }
-                else if (color[v] == 'N')
-                {
-                    cuentaAristas[2]++
-                }
-            }
-        }
-
-        color[origen] = 'N' // Marcarlo como Visitado
-    }
-
-    fun recorridoAnchura(origen: Int = this.origen)
-    {
-        val cola = ArrayDeque<Int>()
-        var  v: Int
-        var adyacentes: List<Int>
-        color[origen] = 'G'
-        distancia[origen] = 0
-        cola.addLast(origen)
-
-        while (cola.isNotEmpty())
-        {
-            v = cola.removeFirst()
-            adyacentes = Adyacentes(v)
-
-            for (w in adyacentes)
-            {
-                if (color[w] == 'B')
-                {
-                    color[w] = 'G'
-                    cuenta++
-                    cuentaAristas[0]++
-                    predecesor[w] = v
-                    distancia[w] = distancia[v] + 1
-                    cola.addLast(w)
-                }
-                else
-                {
-                    if (color[w] == 'G' && v == predecesor[w])
-                    {
-                        cuentaAristas[1]++
-                    }
-                    else if (color[w] == 'N' && v == predecesor[w])
-                    {
-                        cuentaAristas[2]++
-                    }
-                }
-            }
-
-            color[v] = 'N'
-        }
-    }
-
-    fun contarAristas()
-    {
-        var arbol = 0
-        var retorno = 0
-        var avanceCruzada = 0
-
-        for (c in color)
-        {
-            if (c == 'B')
-            {
-                arbol++
-            }
-
-            if (c == 'G')
-            {
-                retorno++
-            }
-
-            if (c == 'N')
-            {
-                avanceCruzada++
-            }
-        }
-        println("Arbol: ${cuentaAristas[0]}")
-        println("Retorno: ${cuentaAristas[1]}")
-        println("Avance/Cruzada: ${cuentaAristas[2]}")
-    }
-
-    fun mostrarCamino(destino: Int)
-    {
-        if (destino == origen)
-            print(origen)
-        else
-        {
-            if (predecesor[destino] == -1)
-            {
-                println("no existe camino desde $destino al origen")
-            }
-            else
-            {
-                mostrarCamino(predecesor[destino])
-                print("->")
-                print(destino)
-            }
-        }
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -2,15 +2,40 @@ fun main(args: Array<String>) {
     if (args.isNotEmpty())
     {
         val grafo = GrafoLista(args[0])
-        grafo.recorridoAnchura()
-        grafo.contarAristas()
+        var titulo = "Grafo"
+        println("\u001B[1m$titulo\u001B[0m")
+        grafo.mostrarGrafo()
+        println()
 
-        for (v in 0..<grafo.ady.size)
+        titulo ="Anchura"
+        println("\u001B[1m$titulo\u001B[0m")
+        val rAnch = grafo.Recorrido()
+        rAnch.anchura()
+        rAnch.clasificarAristas()
+
+        titulo = "Caminos"
+        println("\u001B[1m$titulo\u001B[0m")
+        for (v in 0..< grafo.ady.size)
         {
-            grafo.mostrarCamino(v)
+            println("Camino desde ${rAnch.origen} hasta $v")
+            rAnch.mostrarCamino(v)
             println()
         }
 
+        titulo = "\nProfundidad"
+        println("\u001B[1m$titulo\u001B[0m")
+        val rProf = grafo.Recorrido()
+        rProf.profundidad()
+        rProf.clasificarAristas()
+
+        titulo = "Caminos"
+        println("\u001B[1m$titulo\u001B[0m")
+        for (v in 0..< grafo.ady.size)
+        {
+            println("Camino desde ${rProf.origen} hasta $v")
+            rProf.mostrarCamino(v)
+            println()
+        }
     }
     else
     {
